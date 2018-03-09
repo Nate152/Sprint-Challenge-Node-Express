acde6728b44e9cd34132eb6147667a23a359cc12
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-03-09 13:14:29 -0500

    Functioning Route for Compare

--- src/app.js
+++ src/app.js
@@ -1,10 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const server = express();
-const PORT = 3000;
+const PORT = 5000;
+const STATUS_SUCCESS = 200;
+const STATUS_USER_ERROR = 422;
 
-server.use(body-parser());
+server.use(bodyParser());
+
+server.get('/compare', (req, res) => {
+    fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(res => res.json())
+    .then(json => {
+        let currentRateStr = (json.bpi.USD.rate).toString();
+        currentRate = currentRateStr.replace(/\,/g,"");
+        console.log(currentRate);
+        fetch('https://api.coindesk.com/v1/bpi/historical/close.json?for=yesterday')
+        .then(res => res.json())
+        .then(json => {
+            let historicalRate = (json.bpi['2018-03-08']);
+            console.log(historicalRate);
+            const difference = currentRate - historicalRate;
+            console.log(difference);
+            res.status(STATUS_SUCCESS);
+            res.send(`The difference is : ${difference}`);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(STATUS_USER_ERROR);
+            res.send( { err: err} );
+    })
+    .catch(err => {
+        console.error(err);
+        console.log(err);
+            res.status(STATUS_USER_ERROR);
+            res.send( { err: err} );
+        })
+    })
+})
 
 server.listen(PORT, (err) => {
     if (err) {
