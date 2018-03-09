8d3ae9a9b1d6c0529a3f6b98790521e08303f48f
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-03-09 11:18:39 -0500

    First Commit, Server initialized, dependencies installed, starting paths

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const server = express();
+const PORT = 3000;
+
+server.use(body-parser());
+
+server.listen(PORT, (err) => {
+    if (err) {
+        console.log(`There was an error starting the server: ${err}`);
+    } else {
+        console.log(`Server is listening on port ${PORT}`);
+    }
+})
